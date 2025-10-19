7efdc771780b49852bd259bb755cb6c2c1e41162
Collin Driscoll <provinite@gmail.com>
2025-10-19 17:50:52 -0500

    fix(items): add forwardRef to ItemsModule for circular dependency
    Both UsersModule and ItemsModule now use forwardRef when importing each other
    to properly handle the circular dependency.

--- apps/backend/src/items/items.module.ts
+++ apps/backend/src/items/items.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { DatabaseModule } from '../database/database.module';
 import { CommunitiesModule } from '../communities/communities.module';
 import { UsersModule } from '../users/users.module';
@@ -6,7 +6,7 @@ import { ItemsService } from './items.service';
 import { ItemsResolver } from './items.resolver';
 
 @Module({
-  imports: [DatabaseModule, CommunitiesModule, UsersModule],
+  imports: [DatabaseModule, CommunitiesModule, forwardRef(() => UsersModule)],
   providers: [ItemsService, ItemsResolver],
   exports: [ItemsService],
 })
